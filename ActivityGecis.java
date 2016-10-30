cb53a81032f0fcc1835ebf1a7dd0497d8656a5f1
Elif Damla Küçük <edamlakucuk@gmail.com>
2016-10-31 00:44:59 +0200

    Create ActivityGecis.java

---
+++ ActivityGecis.java
@@ -0,0 +1,17 @@
+package com.example.user.deneme;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class ActivityGecis extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gecis);
+    }
+
+}
